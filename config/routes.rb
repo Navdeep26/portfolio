69d2a067f8709bd63251c30c9f82df3d46cb2198
Navdeep <navdeepkumar@Navdeeps-MacBook-Pro.local>
2017-09-10 13:01:52 +0530

    Integrated skills via model generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home' => "pages#home"
+
+  get 'pages/about' => "pages#about"
+
+  get 'pages/contact' => "pages#contact"
+
   resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
